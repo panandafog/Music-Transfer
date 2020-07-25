0f13b4113a125a0efc81d560d306b4141d7a1d97
panandafog <panandafog@gmail.com>
2020-07-25 15:26:11 +0300

    Initial Commit

---
+++ Music Transfer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Music Transfer
+//
+//  Created by panandafog on 25.07.2020.
+//  Copyright © 2020 panandafog. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
